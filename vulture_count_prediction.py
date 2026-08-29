e4043c5f3519647cbfeea7e1d76c0376f3122168
Divya Prakash <om.prakashpatna18@gmail.com>
2026-08-29 11:50:33 +0530

    Update vulture_count_prediction.py

--- vulture_count_prediction.py
+++ vulture_count_prediction.py
@@ -1,1 +1,11 @@
-
+import pandas as pd
+import numpy as np
+import joblib
+from pydantic import BaseModel
+try:
+  model=joblib.load("knn_model")
+except Exception as e:
+  print(e)
+  return "Model Not Found
+class vulture:
+  
