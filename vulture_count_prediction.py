4a8ff08febdf516df7111371d6dd80ad6cf6464e
Divya Prakash <om.prakashpatna18@gmail.com>
2026-08-29 13:05:01 +0530

    Fix model loading and enhance data preprocessing
    Updated model loading error handling and DataFrame initialization. Improved data preprocessing function to handle missing model and added type casting for latitude and longitude.

--- vulture_count_prediction.py
+++ vulture_count_prediction.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import numpy as np
 import joblib
-from pydantic import BaseModel
+
 try:
   model=joblib.load("knn_model")
 except Exception as e:
   print(e)
-  return "Model Not Found
+  model=None
 
 columns=['decimalLatitude', 'decimalLongitude', 'year', 'Arunachal Pradesh',
        'Assam', 'Bihar', 'Chandigarh', 'Chhattisgarh', 'Delhi', 'Goa',
@@ -18,25 +18,29 @@ columns=['decimalLatitude', 'decimalLongitude', 'year', 'Arunachal Pradesh',
        'Gyps himalayensis', 'Gyps indicus', 'Gyps tenuirostris', 'month_sin',
        'month_cos', 'day_sin', 'day_cos']
 
-vulture_df=pd.DataFrame(data=np.array([0]*len(columns),columns=columns)
+vulture_df=pd.DataFrame(data=np.zeros(1,len(columns)),columns=columns)
                         
 def data_preprocessor(lat,lon,year,state,species,month,day):
+  if model is None:
+    return {"meesage":"Software not working right now. Please try again later."}
   try:
-    vulture_df['decimalLatitude']=lat
-    vulture_df['decimalLongitude']=lon
+    vulture_df['decimalLatitude']=float(lat)
+    vulture_df['decimalLongitude']=float(lon)
     vulture_df['year']=year
-    vulture_df[state]=1
-    vulture_df[species]=1
+    if state in df.columns:
+       vulture_df[state]=1
+    if species in df.columns:
+       vulture_df[species]=1
     vulture_df['month_sin']=np.sin((2*np.pi*month)/12)
     vulture_df['month_cos']=np.cos((2*np.pi*month)/12)
     vulture_df['day_sin']=np.sin((2*np.pi*day)/31)
     vulture_df['day_cos']=np.cos((2*np.pi*day)/31)
     count=model.predict(vulture_df)
     final_counts = np.round(np.expm1(count)).astype(int)
-    return {"predicted_count":final_counts}
+    return {"predicted_count":final_counts[0]}
   except Exception as e:
     print(e)
-    return "Software not working right now. Please try again later."
+    return {"message": "Software not working right now. Please try again later."}
 
   
   
